2759b59693308bc406ab2ed20e671bf956420648
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-17 16:21:17 +0200

    Add localstorage for results

--- src/reducers/resultsReducer.js
+++ src/reducers/resultsReducer.js
@@ -1,10 +1,22 @@
 import { ADD_RESULT, CLEAR_RESULTS } from '../actions/types'
 
-export default (state = [], action) => {
+const storedResults = localStorage.getItem('results')
+
+let initialState = null
+
+if (storedResults) {
+   initialState = JSON.parse(storedResults)
+} else {
+   initialState = []
+}
+
+export default (state = initialState, action) => {
    switch (action.type) {
       case ADD_RESULT:
+         localStorage.setItem('results', JSON.stringify([...state, action.payload]))
          return [...state, action.payload]
       case CLEAR_RESULTS:
+         localStorage.setItem('results', JSON.stringify([]))
          return []
       default:
          return state
