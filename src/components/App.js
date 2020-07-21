74722aaf6f2f09440fb4dd671b75925701b8ec5e
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-21 12:43:19 +0200

    Comment out Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import CssBaseLine from '@material-ui/core/CssBaseline'
 
 import Container from '@material-ui/core/Container'
 
-import NavBar from './NavBar'
+// import NavBar from './NavBar'
 import DiceRoller from './DiceRoller'
-import AbilityScoreRoller from './AbilityScoreRoller'
+// import AbilityScoreRoller from './AbilityScoreRoller'
 
 class App extends Component {
    render() {
