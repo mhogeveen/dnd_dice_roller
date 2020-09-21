428c67115245b0575c54a1902ab1b322bbf1ccb4
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-21 17:15:30 +0200

    Updates

---
+++ precache-manifest.9954d97318e0aca9d22ceaccf0f9e339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ca01f1b19b237dd7b32ac4df51d3e7",
+    "url": "/dnd_dice_roller/index.html"
+  },
+  {
+    "revision": "b6e7c59af22dbe2ccbc2",
+    "url": "/dnd_dice_roller/static/css/main.b8c3bc0e.chunk.css"
+  },
+  {
+    "revision": "43c1ff7471139a9fef69",
+    "url": "/dnd_dice_roller/static/js/2.cddc3015.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/dnd_dice_roller/static/js/2.cddc3015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e7c59af22dbe2ccbc2",
+    "url": "/dnd_dice_roller/static/js/main.5e133836.chunk.js"
+  },
+  {
+    "revision": "14e81548462e000c8d9c",
+    "url": "/dnd_dice_roller/static/js/runtime-main.1d9c4e2c.js"
+  }
+]);
