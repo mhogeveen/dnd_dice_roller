30aa969369f664dd1eacd555b48acb2db538f90b
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-21 12:50:24 +0200

    Use uuidv4 to generate dice id

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const initialState = {
+   [uuidv4()]: {
+      name: '',
+      edit: false,
+      dieAmount: 1,
+      dieType: 20,
+      modAmount: 0,
+   },
+}
+
+export const initialDie = {
+   name: '',
+   edit: false,
+   dieAmount: 1,
+   dieType: 20,
+   modAmount: 0,
+}
--- src/reducers/diceReducer.js
+++ src/reducers/diceReducer.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import {
    UPDATE_DIE_AMOUNT,
    UPDATE_DIE_TYPE,
@@ -7,32 +8,15 @@ import {
    ADD_ROW,
    TOGGLE_EDIT,
 } from '../actions/types'
+import { initialState, initialDie } from '../constants'
 
-const initialState = {
-   0: {
-      name: '',
-      edit: false,
-      dieAmount: 1,
-      dieType: 20,
-      modAmount: 0,
-   },
-}
+// let id = Object.keys(initialState).length - 1
+// const nextId = () => {
+//    id++
+//    return id
+// }
 
-const initialDie = {
-   name: '',
-   edit: false,
-   dieAmount: 1,
-   dieType: 20,
-   modAmount: 0,
-}
-
-let id = Object.keys(initialState).length - 1
-const nextId = () => {
-   id++
-   return id
-}
-
-const savedState = JSON.parse(localStorage.getItem('state'))
+const savedState = JSON.parse(localStorage.getItem('dnd_dice_roller'))
 
 const stateOnLoad = () => {
    if (savedState === null) {
@@ -49,40 +33,40 @@ export default (state = stateOnLoad(), action) => {
             ...state,
             [action.id]: { ...state[action.id], dieAmount: action.payload },
          }
-         localStorage.setItem('state', JSON.stringify(stateDieAmount))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateDieAmount))
          return stateDieAmount
       case UPDATE_DIE_TYPE:
          const stateDieType = {
             ...state,
             [action.id]: { ...state[action.id], dieType: action.payload },
          }
-         localStorage.setItem('state', JSON.stringify(stateDieType))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateDieType))
          return stateDieType
       case UPDATE_MOD_AMOUNT:
          const stateModAmount = {
             ...state,
             [action.id]: { ...state[action.id], modAmount: action.payload },
          }
-         localStorage.setItem('state', JSON.stringify(stateModAmount))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateModAmount))
          return stateModAmount
       case UPDATE_NAME:
          const stateName = { ...state, [action.id]: { ...state[action.id], name: action.payload } }
-         localStorage.setItem('state', JSON.stringify(stateName))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateName))
          return stateName
       case REMOVE_ROW:
          const { [action.id]: _, ...stateRemoveRow } = state
-         localStorage.setItem('state', JSON.stringify(stateRemoveRow))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateRemoveRow))
          return stateRemoveRow
       case ADD_ROW:
-         const stateAddRow = { ...state, [nextId()]: { ...initialDie } }
-         localStorage.setItem('state', JSON.stringify(stateAddRow))
+         const stateAddRow = { ...state, [uuidv4()]: { ...initialDie } }
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateAddRow))
          return stateAddRow
       case TOGGLE_EDIT:
          const stateToggleEdit = {
             ...state,
             [action.id]: { ...state[action.id], edit: !state[action.id].edit },
          }
-         localStorage.setItem('state', JSON.stringify(stateToggleEdit))
+         localStorage.setItem('dnd_dice_roller', JSON.stringify(stateToggleEdit))
          return stateToggleEdit
       default:
          return state
