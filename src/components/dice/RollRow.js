b50500e0b2c2ef8d3ff86f3fb2bd169aa80fcd57
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-21 12:05:33 +0200

    Refactor RollRow to functional comp

--- src/components/dice/RollRow.js
+++ src/components/dice/RollRow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import DieAmount from './DieAmount'
@@ -7,9 +7,9 @@ import ModAmount from './ModAmount'
 import Delete from './Delete'
 import Edit from './Edit'
 
-class RollRow extends Component {
-   renderRollRow = (dieAmount, dieType, modAmount, id) => {
-      if (this.props.edit) {
+const RollRow = ({ edit, dieAmount, dieType, modAmount, id, handleRoll }) => {
+   const renderRollRow = () => {
+      if (edit) {
          return (
             <div className='dice'>
                <div className='dice-comp'>
@@ -26,13 +26,13 @@ class RollRow extends Component {
                </div>
                <div className='dice-comp'>
                   <Delete id={id} />
-                  <Edit edit={this.props.edit} id={id} />
+                  <Edit edit={edit} id={id} />
                </div>
             </div>
          )
       } else {
          return (
-            <div className='dice dice-clickable' onClick={this.props.handleRoll}>
+            <div className='dice dice-clickable' onClick={(e) => handleRoll(e)}>
                <div className='dice-comp'>
                   <span className='dice-comp-info'>{dieAmount}</span>
                </div>
@@ -46,17 +46,14 @@ class RollRow extends Component {
                   <span className='dice-comp-info'>{modAmount}</span>
                </div>
                <div className='dice-comp'>
-                  <Edit edit={this.props.edit} id={id} />
+                  <Edit edit={edit} id={id} />
                </div>
             </div>
          )
       }
    }
 
-   render() {
-      const { dieAmount, dieType, modAmount, id } = this.props
-      return <>{this.renderRollRow(dieAmount, dieType, modAmount, id)}</>
-   }
+   return <>{renderRollRow()}</>
 }
 
 const mapStateToProps = (state, ownProps) => {
