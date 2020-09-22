e8883eac2c056814ef4b6cf27b446e092c345eeb
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-09-22 14:17:44 +0200

    Add TypePicker component

---
+++ src/components/abilityScoreRoller/TypePicker.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { diceTypes } from '../../assets'
+
+import SvgIcon from '@material-ui/core/SvgIcon'
+import IconButton from '@material-ui/core/IconButton'
+import { withStyles } from '@material-ui/styles'
+
+const styles = {
+   root: {
+      color: '#43AA8B',
+   },
+}
+
+const TypePicker = ({ classes, pickedType, setPickedType }) => {
+   const renderPicked = () => {
+      return diceTypes
+         .filter((item) => item.type >= 6 && item.type <= 12)
+         .map((item) => (
+            <IconButton
+               key={item.type}
+               onClick={() => setPickedType(item.type)}
+               className={pickedType === item.type ? classes.root : null}>
+               <SvgIcon viewBox='0 0 24 24' style={{ width: '20px', height: '20px' }}>
+                  <path d={item.icon} />
+               </SvgIcon>
+            </IconButton>
+         ))
+   }
+
+   return (
+      <div className='type-picker'>
+         <h3>What is your characters hit dice type?</h3>
+         <div className='type-picker__buttons'>{renderPicked()}</div>
+      </div>
+   )
+}
+
+export default withStyles(styles)(TypePicker)
--- src/pages/AbilityScoreRoller.js
+++ src/pages/AbilityScoreRoller.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react'
-import { diceTypes } from '../assets'
 
-import SvgIcon from '@material-ui/core/SvgIcon'
-import IconButton from '@material-ui/core/IconButton'
+import { TypePicker } from '../components/abilityScoreRoller'
 
 const AbilityScoreRoller = () => {
    const [pickedType, setPickedType] = useState(null)
 
-   return diceTypes
-      .filter((item) => item.type >= 6 && item.type <= 12)
-      .map((item) => (
-         <IconButton key={item.type} onClick={() => setPickedType(item.type)}>
-            <SvgIcon viewBox='0 0 24 24' style={{ width: '20px', height: '20px' }}>
-               <path d={item.icon} />
-            </SvgIcon>
-         </IconButton>
-      ))
+   return <TypePicker pickedType={pickedType} setPickedType={setPickedType} />
 }
 
 export default AbilityScoreRoller
